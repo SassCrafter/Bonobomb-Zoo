45170027202c13db6824d4fee2dff733024f2ff2
Dima <webbootcamp2020@gmail.com>
2021-03-12 21:19:14 +0200

    fixed plans img on desktop, added prices section

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import '../sass/style.scss';
 
 import videojs from 'video.js';
 import '../../node_modules/video.js/dist/video-js.min.css';
+import  './vendors/swiperInit.js';
 
 import NavMenu from './NavMenu';
 
